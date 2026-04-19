f40a893918eb8349a15eda17bd82f79e59fcef0d
Pitur Cristian <195602401+OverStacked-Dev@users.noreply.github.com>
2026-04-19 18:23:07 +0300

    Fix MAIN_URL to point to the correct main.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
-local MAIN_URL = "https://raw.githubusercontent.com/OverStacked-Dev/MagnetoZz/main/MagnetoZz/main.lua"
+local MAIN_URL = "https://raw.githubusercontent.com/OverStacked-Dev/MagnetoZz/main/main.lua"
 
 local source = game:HttpGet(MAIN_URL)
 loadstring(source)()
